fbca12077feb609e51404abfc557e52959900544
dev-lesa <luiseduardosa2006@gmail.com>
2025-04-30 22:04:54 -0500

    Problema.java

--- Semana3/src/semana3/Problema.java
+++ Semana3/src/semana3/Problema.java
@@ -36,6 +36,21 @@ public class Problema {
         presentarArreglo(arreglo);
         int[] arregloOrdenado = ordenarBurbuja(arreglo);
         presentarArreglo(arregloOrdenado);
+        
+        System.out.println("Ingrese el valor a eliminar:");
+        int valorEliminar = entrada.nextInt();
+        arregloOrdenado = eliminarValor(arregloOrdenado, valorEliminar);
+        System.out.println("Arreglo después de eliminar:");
+        presentarArreglo(arregloOrdenado);
+        
+        System.out.println("Ingrese el valor a sustituir:");
+        int valorAntiguo = entrada.nextInt();
+        System.out.println("Ingrese el nuevo valor:");
+        int valorNuevo = entrada.nextInt();
+        arregloOrdenado = sustituirValor(arregloOrdenado, valorAntiguo, valorNuevo);
+        
+        System.out.println("Arreglo final:");
+        presentarArreglo(arregloOrdenado);
     }
 
     public static boolean verificar(int[] arreglo, int num, int contador) {
@@ -75,6 +90,47 @@ public class Problema {
                 break;
             }
         }
+        return arreglo;
+    }
+    
+    public static int[] eliminarValor(int[] arreglo, int valor) {
+        int[] nuevoArreglo = new int[arreglo.length - 1];
+        int index = 0;
+        boolean encontrado = false;
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valor && !encontrado) {
+                encontrado = true;
+                continue;
+            }
+            if (index < nuevoArreglo.length) {
+                nuevoArreglo[index] = arreglo[i];
+                index++;
+            }
+        }
+        
+        if (!encontrado) {
+            System.out.println("El valor no se encontró en el arreglo.");
+            return arreglo;
+        }
+        
+        return nuevoArreglo;
+    }
+    
+    public static int[] sustituirValor(int[] arreglo, int valorAntiguo, int valorNuevo) {
+        boolean encontrado = false;
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valorAntiguo) {
+                arreglo[i] = valorNuevo;
+                encontrado = true;
+            }
+        }
+        
+        if (!encontrado) {
+            System.out.println("El valor a sustituir no se encontró en el arreglo.");
+        }
+        
         return arreglo;
     }
 }
